288a64a3797028b96ef213ed536b406b39e69b04
MariusRusaas <61704007+MariusRusaas@users.noreply.github.com>
2026-01-06 18:34:54 +0100

    Rigid registration pipeline

---
+++ RigidReg/RigidReg.m
@@ -0,0 +1,95 @@
+function registered = RigidReg(ref, srce, other, interpol)
+    %SPMRESCLICE Summary of this function goes here
+    %   Detailed explanation goes here
+    if nargin < 2
+        other = [];
+    end
+    if nargin < 3
+        interpol = [];
+    end
+
+    [refFile, files] = copyFiles(ref, srce, other);
+
+    if ~isempty(interpol)
+        if ~isnumeric(interpol) || numel(interpol) ~= numel(other)
+            error('The "interpol" argument must be a numeric vector with the same number of elements as "other".');
+        end
+    end
+    N = numel(files);
+    inter = zeros(1, N);
+    inter(1) = 4;
+
+    if ~isempty(interpol)
+        inter(2:end) = 0;
+        inter(2:end) = interpol;
+    end
+
+
+    matlabbatch{1}.spm.spatial.coreg.estimate.ref = cellstr(refFile);
+    matlabbatch{1}.spm.spatial.coreg.estimate.source = files(1);
+    matlabbatch{1}.spm.spatial.coreg.estimate.other = files(2:end);
+    matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.cost_fun = 'nmi'; % Normalized Mutual Information
+    matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.sep = [4 2]; % Optimization steps
+    matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.tol = [0.0200 0.0200 0.0200 0.0010 0.0010 0.0010 0.0100 0.0100 0.0100 0.0010 0.0010 0.0010]; 
+    matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.fwhm = [4 4]; % Gaussian smoothing
+
+    spm_jobman('run', matlabbatch);
+
+    for i = 2:numel(files)
+        P = char(refFile, files{i});  % Reference first
+        spm_reslice(P, struct('interp', double(inter(i)), 'mask', 0, 'which', 1, 'mean', 0, 'prefix', ''));
+    end
+    
+    registered = files;
+end
+
+
+function [refFile, files] = copyFiles(ref, srce, others)
+    [path,~,~] = fileparts(ref);
+    path = fullfile(path, 'RigidReg');
+    if ~exist(path, 'dir'), mkdir(path) 
+    end
+    refFile = copyFile(fullfile(ref), path, 'reference.nii');
+
+    files = {};
+    files = [files; copyFile(fullfile(srce),path)];
+    if ~isempty(others)
+        for i = 1:length(others)
+            if ~exist(others{i}, 'file'),error('The file %s does not exist.', others{i}); end
+           files = [files; copyFile(char(others(i)), path)];
+        end
+    end
+
+
+end
+
+
+function filename = copyFile(root, newpath, rename)
+    if nargin < 3
+        rename = [];
+    end
+
+    [~,name,ext] = fileparts(root);
+    if contains(name, '.nii') && strcmp(ext, '.gz')
+        if ~exist(newpath, 'dir'), mkdir(newpath); end
+        filenames = gunzip(root, newpath);
+        filename = filenames{1};
+        % Rename if requested
+        if ~isempty(rename)
+            newFilename = fullfile(newpath, rename);
+            movefile(filename, newFilename);
+            filename = newFilename;
+        end
+    elseif strcmp(ext, '.nii')
+        if ~isempty(rename)
+            filename = fullfile(newpath, rename);
+        else
+            filename = fullfile(newpath, strcat(name,ext));
+        end
+        copyfile(root, filename)
+    else
+        error('The file %s is not a valid NIfTI file.', root);
+    end
+
+
+end
---
+++ RigidReg/RigidReg.py
@@ -0,0 +1,53 @@
+import SimpleITK as sitk
+import numpy as np
+import matlab.engine
+import os
+import argparse
+
+def RigidReg(refPath, srcPath, other=None, inter=None, verbose=False):
+    """
+    Main function to run the RigidReg pipeline.
+
+    Parameters:
+        refPath (str): Path to the Reference image for registration.
+        srcPath (str): Path to the Source image to be registered.
+        outputPath (str, optional): Path to save the output image. Defaults to None.
+        other (optional): Additional imges to co-register (must be in MNI space).
+        inter (optional): Interpolation method for co-registration of additional images in other.
+
+    Returns:
+        str: Path to the output image files.
+    """
+    if verbose: print("################ Running Rigid Registation ################  \n")
+    eng = matlab.engine.start_matlab()
+    currDir = os.path.dirname(os.path.abspath(__file__))
+    eng.addpath(currDir, nargout=0)
+
+    if other is not None:
+        others = {othr for othr in other}
+        if inter is not None: resliced = eng.RigidReg(refPath, srcPath, others, np.array(inter))
+        else: resliced = eng.RigidReg(refPath, srcPath, others)
+    else: resliced = eng.RigidReg(refPath, srcPath)
+
+    if verbose: print(f"\n################ Rigid Registration finished! ################  \n")
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Run RigidReg pipeline for brain segmentation and registration.")
+    parser.add_argument("--refPath", type=str, required=True, help="Path to the input image file.")
+    parser.add_argument("--srcPath", type=str, required=True, help="Path to the CT image file.")
+    parser.add_argument("--other", type=str, nargs='*', default=None, help="Additional images to co-register (must be in MNI space).")
+    parser.add_argument("--inter", type=int, nargs='*', default=None, help="Interpolation method for co-registration of additional images.")
+    parser.add_argument("--verbose", action='store_true', help="Enable verbose output.")
+
+    args = parser.parse_args()
+    
+    RigidReg(args.refPath, args.srcPath, 
+            other=args.other,
+            inter=args.inter, 
+            verbose=args.verbose, )
+
+
+if __name__ == "__main__":
+    main()
